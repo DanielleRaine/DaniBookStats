033228c9839904c2f7ca5e9969d76f559945af9a
Dani Hassanieh <dani.hassanieh@outlook.com>
2024-03-29 14:06:29 -0500

    Starting work on data fetching!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import requests
+from math import ceil
 
 # load .env file that contains api key
 load_dotenv()
@@ -10,7 +11,8 @@ API_KEY = os.getenv('GOOGLE_BOOKS_API_KEY')
 headers = {"Accept-Encoding": "gzip",
            "User-Agent": "DaniBookStats/0.1 (https://github.com/DanielleHassanieh/DaniBookStats)"}
 # list of genres to get data from
-genres = ["Romance", "Mystery", "Textbook"]
+genres = ["Horror", "Mystery"]
+#genres = ["Romance", "Mystery", "Fantasy", "Science Fiction", ""]
 
 
 def get_num_books_subject(subject):
@@ -24,14 +26,32 @@ def get_num_books_subject(subject):
 
 # "startIndex": "0", "maxResults": "1"
 r = requests.get('https://www.googleapis.com/books/v1/volumes',
-                 params={'q': 'subject:"Romance"', 'key': API_KEY},
+                 params={'q': 'subject:"Mystery"+inauthor:"Kenneth H. Rosen"', 'key': API_KEY},
                  headers=headers)
+
+print(40)
+
+for g in genres:
+
+    num_queries = ceil(get_num_books_subject(g) / 40)
+
+    for i in range(num_queries):
+         r = requests.get('https://www.googleapis.com/books/v1/volumes', params={'q': 'subject:"Mystery"+inauthor:"Kenneth H. Rosen"', 'key': API_KEY}, headers=headers)
+
+
+    if r.ok:
+        pass
+
+   # pass
+
+
+#r = requests.get("https://www.google.com/search?q=subject:%22Fiction%22&sca_esv=09842260a7ca25e4&sca_upv=1&tbm=bks&sxsrf=ACQVn08DDS_c0GBQ_837Xe0dRnh0c-Qmtw:1711732756530&ei=FPgGZuuFIJbKkPIPsqO52A8&start=0&sa=N&ved=2ahUKEwjr35y3_ZmFAxUWJUQIHbJRDvs4WhDy0wN6BAgPEAQ&biw=1920&bih=932&dpr=1", params={'key': API_KEY}, headers=headers)
 
 #d = dict(r.json())
 
 #d.update()
 
-print(get_num_books_subject("Romance"))
+#print(get_num_books_subject("Fiction"))
 
 with open("bob.json", "w") as f:
     f.write(r.text)
