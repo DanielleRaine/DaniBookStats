b4a0f8b702ac67a86df4a26f5583ad439adfb215
Dani Hassanieh <dani.hassanieh@outlook.com>
2024-03-29 01:32:05 -0500

    new function that counts total items found by api query

--- main.py
+++ main.py
@@ -2,18 +2,36 @@ import os
 from dotenv import load_dotenv
 import requests
 
-# load .env file
+# load .env file that contains api key
 load_dotenv()
-# get api key
+# get api key to authenticate with google books api
 API_KEY = os.getenv('GOOGLE_BOOKS_API_KEY')
-# headers
-headers = {"Accept-Encoding": "gzip", "User-Agent": "DaniBookStats/0.1 (https://github.com/DanielleHassanieh/DaniBookStats)"}
+# enable gzip compression and state user agent
+headers = {"Accept-Encoding": "gzip",
+           "User-Agent": "DaniBookStats/0.1 (https://github.com/DanielleHassanieh/DaniBookStats)"}
+# list of genres to get data from
+genres = ["Romance", "Mystery", "Textbook"]
 
-# request
+
+def get_num_books_subject(subject):
+    return requests.get('https://www.googleapis.com/books/v1/volumes',
+                        params={'q': f'subject:"{subject}"',
+                                'key': API_KEY,
+                                'fields': "totalItems",
+                                'printType': 'books'},
+                        headers=headers).json()["totalItems"]
+
+
+# "startIndex": "0", "maxResults": "1"
 r = requests.get('https://www.googleapis.com/books/v1/volumes',
-                 params={'q': 'inauthor:Stephen+inauthor:King', 'key': API_KEY, "startIndex": "0", "maxResults": "10"},
+                 params={'q': 'subject:"Romance"', 'key': API_KEY},
                  headers=headers)
 
+#d = dict(r.json())
+
+#d.update()
+
+print(get_num_books_subject("Romance"))
 
 with open("bob.json", "w") as f:
     f.write(r.text)
