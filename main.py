f50de7c35f2b3edba6b8b07168dd832b2f3c40e0
Danielle Hassanieh <dani.hassanieh@outlook.com>
2024-04-02 20:48:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,61 +2,52 @@ import os
 from dotenv import load_dotenv
 import requests
 from math import ceil
-from time import sleep
+import mysql.connector as mariadb
 
 # load .env file that contains api key
 load_dotenv()
-# get api key to authenticate with google books api
+# get api key to authenticate with Google Books api
 API_KEY = os.getenv('GOOGLE_BOOKS_API_KEY')
 # enable gzip compression and state user agent
-headers = {"Accept-Encoding": "gzip",
-           "User-Agent": "DaniBookStats/0.1 (https://github.com/DanielleHassanieh/DaniBookStats)"}
+headers = {'Accept-Encoding': 'gzip',
+           'User-Agent': 'DaniBookStats/0.1 (https://github.com/DanielleHassanieh/DaniBookStats)'}
 # list of genres to get data from
-genres = ["Horror", "Mystery"]
+genres = ['Horror', 'Mystery']
 
+# create connection to database
+mariadb_connection = mariadb.connect(user=os.getenv('MARIADB_USER'),
+                                     password=os.getenv('MARIADB_PASSWORD'),
+                                     host=os.getenv('MARIADB_HOST'),
+                                     port=os.getenv('MARIADB_PORT'))
+
+mariadb_cursor = mariadb_connection.cursor()
+
+mariadb_cursor.execute("SHOW DATABASES")
+
+for x in mariadb_cursor:
+    print(x)
 
 # genres = ["Romance", "Mystery", "Fantasy", "Science Fiction", ""]
 
 
-def get_subject_num_books(subject):
-    return requests.get('https://www.googleapis.com/books/v1/volumes',
-                        params={'q': f'subject:"{subject}"',
-                                'key': API_KEY,
-                                'fields': "totalItems",
-                                'printType': 'books'},
-                        headers=headers).json()["totalItems"]
-
-
-r = requests.get('https://www.googleapis.com/books/v1/volumes',
-                 params={'q': f'subject:"Mystery"',
-                         'key': API_KEY,
-                         'fields': 'items(id,volumeInfo(title,authors,publishedDate,pageCount,categories,averageRating,ratingsCount))',
-                         'startIndex': 0,
-                         'maxResults': 40},
-                 headers=headers)
-
-# for g in genres:
+# def get_subject_total_items(subject):
+#     return requests.get('https://www.googleapis.com/books/v1/volumes',
+#                         params={'q': f'subject:"{subject}"',
+#                                 'key': API_KEY,
+#                                 'fields': 'totalItems',
+#                                 'printType': 'books'},
+#                         headers=headers).json()['totalItems']
 #
-#     num_queries = ceil(get_subject_num_books(g) / 40)
+#
+# for g in genres:
+#     num_queries = ceil(get_subject_total_items(g) / 40)
 #
 #     for i in range(num_queries):
 #         r = requests.get('https://www.googleapis.com/books/v1/volumes',
 #                          params={'q': f'subject:"{g}"',
 #                                  'key': API_KEY,
-#                                  'fields': 'items(id,volumeInfo(title,authors,publishedDate,pageCount,categories,averageRating,ratingsCount))',
+#                                  'fields': 'items(id,volumeInfo(title,authors,publishedDate,pageCount,categories,'
+#                                            'averageRating,ratingsCount))',
 #                                  'startIndex': i * 40,
 #                                  'maxResults': 40},
 #                          headers=headers)
-#         # if not r.ok:
-#         #     continue
-#
-
-
-# d = dict(r.json())
-
-# d.update()
-
-# print(get_num_books_subject("Fiction"))
-
-with open("bob.json", "w") as f:
-    f.write(r.text)
