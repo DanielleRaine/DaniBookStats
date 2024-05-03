7d230b15ca0f05c632f314c35d6dadf735db340f
Dani Hassanieh <dani.hassanieh@outlook.com>
2024-05-03 14:41:20 -0500

    Progress!

--- bookstats.R
+++ bookstats.R
@@ -3,12 +3,19 @@ install.packages("dotenv")
 install.packages("RMariaDB")
 install.packages("DBI")
 installed.packages("ggplot2")
+install.packages("stringr")
+install.packages("dplyr")
+install.packages("crayon")
+
 
 # load packages
 library(dotenv)
 library(RMariaDB)
 library(DBI)
 library(ggplot2)
+library(stringr)
+library(dplyr)
+library(crayon)
 
 
 # load all the secret data
@@ -33,6 +40,19 @@ dbDisconnect(con)
 genre_count <- as.data.frame(table(c(bookdata$genre)))
 # rename the columns
 names(genre_count) <- c("genre", "count")
-
+# construct bar graph that shows count of each book by genre
 ggplot(genre_count, aes(x=genre, y=count)) +
   geom_bar(stat = "identity")
+
+# count the number if authors per entry using a pipe
+bookdata <- bookdata %>%
+  mutate(num_authors = sapply(strsplit(ifelse(is.na(authors), "", authors), ", "), length))
+
+# calculate summary stats for num authors associated with genre
+num_author_summary_stats <- bookdata %>%
+  filter(num_authors != 0) %>%
+  group_by(genre) %>%
+  summarise(mean_authors = mean(num_authors, na.rm = TRUE),
+            sd_authors = sd(num_authors, na.rm = TRUE))
+
+dnorm(mean = num_author_summary_stats[1,2], sd = num_author_summary_stats[1,3])
